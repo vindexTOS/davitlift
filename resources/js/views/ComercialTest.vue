3a66b780036b0fd98d45083b79ce8062c7fcbf2c
VindexTOS <giorgikutateladze1998@gmail.com>
2024-04-27 22:36:22 +0400

    finished comercial testing probably finished idk

--- resources/js/views/ComercialTest.vue
+++ resources/js/views/ComercialTest.vue
@@ -1,7 +1,7 @@
 <template>
-  <v-container>
+  <div class="custom-container">
     <!-- Line and Area Charts -->
-    <v-row>
+    <!-- <v-row>
       <v-col cols="12" md="6">
         <apexchart
           type="line"
@@ -16,17 +16,11 @@
           :series="areaChartSeries"
         ></apexchart>
       </v-col>
-    </v-row>
+    </v-row> -->
 
     <!-- Bar and Column Charts -->
-    <v-row>
-      <v-col cols="12" md="6">
-        <apexchart
-          type="bar"
-          :options="barChartOptions"
-          :series="barChartSeries"
-        ></apexchart>
-      </v-col>
+    <!-- <v-row>
+    
       <v-col cols="12" md="6">
         <apexchart
           type="column"
@@ -34,17 +28,10 @@
           :series="columnChartSeries"
         ></apexchart>
       </v-col>
-    </v-row>
+    </v-row> -->
 
     <!-- Pie and Donut Charts -->
-    <v-row>
-      <v-col cols="12" md="6">
-        <apexchart
-          type="pie"
-          :options="pieChartOptions"
-          :series="pieChartSeries"
-        ></apexchart>
-      </v-col>
+    <!-- <v-row>
       <v-col cols="12" md="6">
         <apexchart
           type="donut"
@@ -52,10 +39,10 @@
           :series="donutChartSeries"
         ></apexchart>
       </v-col>
-    </v-row>
+    </v-row> -->
 
     <!-- Radar and Polar Area Charts -->
-    <v-row>
+    <!-- <v-row>
       <v-col cols="12" md="6">
         <apexchart
           type="radar"
@@ -70,10 +57,10 @@
           :series="polarAreaChartSeries"
         ></apexchart>
       </v-col>
-    </v-row>
+    </v-row> -->
 
     <!-- Scatter and Bubble Charts -->
-    <v-row>
+    <!-- <v-row>
       <v-col cols="12" md="6">
         <apexchart
           type="scatter"
@@ -88,26 +75,63 @@
           :series="bubbleChartSeries"
         ></apexchart>
       </v-col>
-    </v-row>
+    </v-row> -->
 
-    <!-- Heatmap and Treemap Charts -->
-    <v-row>
-      <v-col cols="12" md="6">
-        <apexchart
-          type="heatmap"
-          :options="heatmapChartOptions"
-          :series="heatmapChartSeries"
-        ></apexchart>
-      </v-col>
-      <v-col cols="12" md="6">
-        <apexchart
-          type="treemap"
-          :options="treemapChartOptions"
-          :series="treemapChartSeries"
-        ></apexchart>
-      </v-col>
-    </v-row>
-  </v-container>
+    <section>
+      <div class="row-gap">
+        <div class="custom-col">
+          <h1 style="margin-left: 30px;">შლაგბაუმი</h1>
+
+          <apexchart
+            type="treemap"
+            :options="treemapChartOptions"
+            :series="treemapChartSeries"
+          ></apexchart>
+        </div>
+        <div class="custom-col">
+          <h1 style="margin-left: 30px;">დომოფონი</h1>
+
+          <apexchart
+            type="treemap"
+            :options="treemapChartOptionsTwo"
+            :series="treemapChartSeriesTwo"
+          ></apexchart>
+        </div>
+      </div>
+
+      <!-- donay -->
+
+      <!-- lll -->
+      <div class="row-gap-card-system">
+        <h1 style="padding-bottom: 20px;">ლიფტის საბარათე სისტემა</h1>
+
+        <div class="row-gap">
+          <div class="custom-col">
+            <h1 style="margin-left: 30px;">ხარჯები</h1>
+
+            <div class="pie-chart">
+              <!-- Adjust width and height as needed -->
+              <apexchart
+                type="pie"
+                :options="pieChartOptions"
+                :series="pieChartSeries"
+              ></apexchart>
+            </div>
+          </div>
+
+          <div class="custom-col">
+            <h1 style="margin-left: 30px;">შემოსავლები</h1>
+
+            <apexchart
+              type="bar"
+              :options="barChartOptions"
+              :series="barChartSeries"
+            ></apexchart>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script>
@@ -120,7 +144,146 @@ export default {
   },
   data() {
     return {
-      // Line Chart
+      // Treemap Chart////////////
+      pieChartOptions: {
+        chart: {
+          type: 'pie',
+          width: '100%', // Set the width to 100% to occupy the entire container
+          height: '100%', // Set the height to 100% to occupy the entire container
+        },
+        labels: [
+          'ტექნიკური მომსახურებ',
+          'დასუფთავება',
+          'განათება',
+          'ეზოს კეთილმოწყობა',
+          'სხვა',
+        ], // Add your custom labels
+      },
+      pieChartSeries: [44, 55, 13, 43, 22],
+      // Bar Chart
+      barChartOptions: {
+        chart: {
+          type: 'bar',
+        },
+        xaxis: {
+          categories: [
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+            'Jul',
+            'Aug',
+            'Sep',
+          ],
+        },
+        plotOptions: {
+          bar: {
+            distributed: true,
+            dataLabels: {
+              enabled: true,
+            },
+          },
+        },
+      },
+      barChartSeries: [
+        {
+          name: 'Series 1',
+          data: [1349, 1559, 1604, 1819, 2123, 1923, 2420, 2493, 2470],
+          color: (data) => {
+            // Define colors for different seasons
+            const winterColors = ['#4292c6', '#3282bf', '#2b73b9']
+            const springColors = ['#6aa84f', '#61a043', '#58983b']
+            const summerColors = ['#ff9933', '#ff8c1a', '#ff8000']
+            const fallColors = ['#e67e22', '#e57c1e', '#e4791a']
+
+            const month = data.x // Get the month category
+
+            // Assign colors based on the season
+            if (month === 'Dec' || month === 'Jan' || month === 'Feb') {
+              return winterColors[data.dataPointIndex % winterColors.length]
+            } else if (month === 'Mar' || month === 'Apr' || month === 'May') {
+              return springColors[data.dataPointIndex % springColors.length]
+            } else if (month === 'Jun' || month === 'Jul' || month === 'Aug') {
+              return summerColors[data.dataPointIndex % summerColors.length]
+            } else {
+              return fallColors[data.dataPointIndex % fallColors.length]
+            }
+          },
+        },
+      ],
+      treemapChartOptions: {
+        chart: {
+          type: 'treemap',
+        },
+        tooltip: {
+          style: {
+            fontSize: '16px', // Font size for tooltip
+          },
+        },
+        plotOptions: {
+          treemap: {
+            dataLabels: {
+              enabled: true,
+              style: {
+                fontFamily: 'Arial, sans-serif', // Font family for treemap text
+                fontSize: '44px', // Font size for treemap text
+                color: 'black', // Text color for treemap text
+              },
+            },
+          },
+        },
+      },
+      treemapChartSeries: [
+        {
+          data: [
+            {
+              x: 'თავისუფალი პარკირება: 8',
+              y: 500,
+              fillColor: '#96d1f8', // Custom color for the first circle
+            },
+            {
+              x: 'გახსენი შლაგბაუმი',
+              y: 300,
+              fillColor: '#6bac8e', // Custom color for the second circle
+            },
+            {
+              x: 'შლაგბაუმი დაკეტილია',
+              y: 400,
+              fillColor: '#ff1b1e', // Custom color for the third circle
+            },
+            {
+              x: 'შევსებული პატკირება:  20',
+              y: 400,
+              fillColor: '#F7EE14', // Custom color for the fourth circle
+            },
+          ],
+        },
+      ],
+      // Treemap Chart/////////////////////////////////////////////////////////////////////////////////////
+      treemapChartOptionsTwo: {
+        chart: {
+          type: 'treemap',
+        },
+      },
+      treemapChartSeriesTwo: [
+        {
+          data: [
+            {
+              x: 'კარი დაკეტილია',
+              y: 500,
+              fillColor: '#e33b26', // Custom color for the first circle
+            },
+            {
+              x: 'გააღე კარი',
+              y: 500,
+              fillColor: '#31c312',
+            },
+          ],
+        },
+      ],
+      //   Line Chart///////////////////////////////////////////////////////////////////////////////////////
       lineChartOptions: {
         chart: {
           type: 'line',
@@ -170,31 +333,7 @@ export default {
           data: [30, 40, 35, 50, 49, 60, 70, 91, 125],
         },
       ],
-      // Bar Chart
-      barChartOptions: {
-        chart: {
-          type: 'bar',
-        },
-        xaxis: {
-          categories: [
-            'Jan',
-            'Feb',
-            'Mar',
-            'Apr',
-            'May',
-            'Jun',
-            'Jul',
-            'Aug',
-            'Sep',
-          ],
-        },
-      },
-      barChartSeries: [
-        {
-          name: 'Series 1',
-          data: [30, 40, 35, 50, 49, 60, 70, 91, 125],
-        },
-      ],
+
       // Column Chart
       columnChartOptions: {
         chart: {
@@ -221,12 +360,7 @@ export default {
         },
       ],
       // Pie Chart
-      pieChartOptions: {
-        chart: {
-          type: 'pie',
-        },
-      },
-      pieChartSeries: [44, 55, 13, 43, 22],
+
       // Donut Chart
       donutChartOptions: {
         chart: {
@@ -357,55 +491,6 @@ export default {
           ],
         },
       ],
-      // Treemap Chart
-      treemapChartOptions: {
-        chart: {
-          type: 'treemap',
-        },
-        tooltip: {
-          style: {
-            fontSize: '16px', // Increase font size for tooltip
-          },
-        },
-        plotOptions: {
-          treemap: {
-            style: {
-              fontSize: '24px', // Increase font size for treemap elements
-            },
-            dataLabels: {
-              style: {
-                fontSize: '28px', // Set font size for text inside treemap boxes
-              },
-            },
-          },
-        },
-      },
-      treemapChartSeries: [
-        {
-          data: [
-            {
-              x: 'თავისუფალი პარკირება: 8',
-              y: 500,
-              fillColor: '#96d1f8', // Custom color for the first circle
-            },
-            {
-              x: 'გახსენი შლაგბაუმი',
-              y: 300,
-              fillColor: '#6bac8e', // Custom color for the second circle
-            },
-            {
-              x: 'შლაგბაუმი დაკეტილია',
-              y: 400,
-              fillColor: '#ff1b1e', // Custom color for the third circle
-            },
-            {
-              x: 'შევსებული პატკირება:  20',
-              y: 400,
-              fillColor: '#F7EE14', // Custom color for the fourth circle
-            },
-          ],
-        },
-      ],
     }
   },
 }
@@ -413,6 +498,76 @@ export default {
 
 <style scoped>
 h1 {
-  font-size: 2em;
+  font-size: 1.2rem;
+  color: rgb(150, 150, 150);
+}
+.custom-container {
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+}
+.pie-chart-container {
+  width: 100%;
+  height: 600px; /* Set the desired height */
+}
+.custom-col {
+  width: 450px;
+  height: 330px;
+  background-color: rgb(249, 249, 249);
+  border-radius: 10px; /* Adjust the radius as needed */
+  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.1); /* Adjust the shadow as needed */
+  padding: 2px; /* Adjust the padding as needed */
+}
+section {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  padding: 20px;
+  gap: 20px;
+}
+.row-gap-card-system {
+  display: flex;
+  align-items: center;
+
+  flex-wrap: wrap;
+  justify-content: center;
+  flex-direction: column;
+  background-color: white;
+  border-radius: 10px; /* Adjust the radius as needed */
+  box-shadow: 0 4px 4px rgba(0, 0, 0, 0.1); /* Adjust the shadow as needed */
+  padding: 2px; /* Adjust the padding as needed */
+}
+.card-system-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+}
+.custom-col-card {
+  width: 450px;
+
+  padding: 2px; /* Adjust the padding as needed */
+}
+
+.row-gap {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+}
+.pie-chart {
+  width: 450px; /* Default width */
+}
+
+@media (max-width: 600px) {
+  .pie-chart {
+    width: 380px; /* Adjusted width for phone screens */
+  }
+  .custom-col {
+    width: 380px;
+    height: 300px;
+    background-color: white;
+    border-radius: 10px; /* Adjust the radius as needed */
+    box-shadow: 0 4px 4px rgba(0, 0, 0, 0.1); /* Adjust the shadow as needed */
+    padding: 2px; /* Adjust the padding as needed */
+  }
 }
 </style>
