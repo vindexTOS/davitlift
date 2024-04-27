c042afb1c969189e0042e442fbccfec41a10b8a8
VindexTOS <giorgikutateladze1998@gmail.com>
2024-04-27 20:15:28 +0400

    testing comerclia page tests

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -1,134 +1,162 @@
 // Composables
-import {createRouter, createWebHistory} from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import store from '../store'
 
 const routes = [
-    {
-        path: '/',
-        component: () => import('../layouts/default/Default.vue'),
-        children: [
-            {
-                path: '',
-                name: 'Home',
-                component: () => import(/* webpackChunkName: "home" */ '../views/Home.vue'),
-            },
-            {
-                path: 'company',
-                name: 'company',
-                component: () => import(/* webpackChunkName: "home" */ '../views/CompanyPage.vue'),
-            },
-            {
-                path: 'user/:id',
-                name: 'userDetail',
-                component: () => import(/* webpackChunkName: "home" */ '../views/UserDetailed.vue'),
-            },
-            {
-                path: 'company/:id',
-                name: 'companyDetail',
-                component: () => import(/* webpackChunkName: "home" */ '../views/CompanyDetailPage.vue'),
-            },
-            {
-                path: 'manager/:id/:companyId',
-                name: 'managerDetail',
-                component: () => import(/* webpackChunkName: "home" */ '../views/ManagerDetailPage.vue'),
-            },
-            {
-                path: 'users',
-                name: 'users',
-                component: () => import(/* webpackChunkName: "home" */ '../views/UsersPage.vue'),
-            },
-            {
-                path: 'devices',
-                name: 'devices',
-                component: () => import(/* webpackChunkName: "home" */ '../views/DevicesPage.vue'),
-            },
-            {
-                path: 'devices/:id',
-                name: 'devicesDetail',
-                component: () => import(/* webpackChunkName: "home" */ '../views/DevicesDetailPage.vue'),
-            },
-            {
-                path: 'dashboard',
-                name: 'dashboard',
-                component: () => import(/* webpackChunkName: "home" */ '../views/DashboardPage.vue'),
-            },
-            {
-                path: 'file',
-                name: 'file',
-                component: () => import(/* webpackChunkName: "home" */ '../views/FileUpload.vue'),
-            },
-            {
-                path: 'unregistreddevices',
-                name: 'unregistreddevices',
-                component: () => import(/* webpackChunkName: "home" */ '../views/UnregistredDevicesPage.vue'),
-            },
-        ],
-        beforeRouteLeave(to, from, next) {
-            return next(false);
-        }
+  {
+    path: '/',
+    component: () => import('../layouts/default/Default.vue'),
+    children: [
+      {
+        path: '',
+        name: 'Home',
+        component: () =>
+          import(/* webpackChunkName: "home" */ '../views/Home.vue'),
+      },
+      {
+        path: '/PageTest',
+        name: 'PageTest',
+        component: () =>
+          import(/* webpackChunkName: "home" */ '../views/ComercialTest.vue'),
+      },
+      {
+        path: 'company',
+        name: 'company',
+        component: () =>
+          import(/* webpackChunkName: "home" */ '../views/CompanyPage.vue'),
+      },
+      {
+        path: 'user/:id',
+        name: 'userDetail',
+        component: () =>
+          import(/* webpackChunkName: "home" */ '../views/UserDetailed.vue'),
+      },
+      {
+        path: 'company/:id',
+        name: 'companyDetail',
+        component: () =>
+          import(
+            /* webpackChunkName: "home" */ '../views/CompanyDetailPage.vue'
+          ),
+      },
+      {
+        path: 'manager/:id/:companyId',
+        name: 'managerDetail',
+        component: () =>
+          import(
+            /* webpackChunkName: "home" */ '../views/ManagerDetailPage.vue'
+          ),
+      },
+      {
+        path: 'users',
+        name: 'users',
+        component: () =>
+          import(/* webpackChunkName: "home" */ '../views/UsersPage.vue'),
+      },
+      {
+        path: 'devices',
+        name: 'devices',
+        component: () =>
+          import(/* webpackChunkName: "home" */ '../views/DevicesPage.vue'),
+      },
+      {
+        path: 'devices/:id',
+        name: 'devicesDetail',
+        component: () =>
+          import(
+            /* webpackChunkName: "home" */ '../views/DevicesDetailPage.vue'
+          ),
+      },
+      {
+        path: 'dashboard',
+        name: 'dashboard',
+        component: () =>
+          import(/* webpackChunkName: "home" */ '../views/DashboardPage.vue'),
+      },
+      {
+        path: 'file',
+        name: 'file',
+        component: () =>
+          import(/* webpackChunkName: "home" */ '../views/FileUpload.vue'),
+      },
+      {
+        path: 'unregistreddevices',
+        name: 'unregistreddevices',
+        component: () =>
+          import(
+            /* webpackChunkName: "home" */ '../views/UnregistredDevicesPage.vue'
+          ),
+      },
+    ],
+    beforeRouteLeave(to, from, next) {
+      return next(false)
     },
-    {
-        beforeRouteLeave(to, from, next) {
-            return next(false);
-        },
-        name: 'Login',
-        path: '/login',
-        component: () => import('../views/LoginPage.vue')
+  },
+  {
+    beforeRouteLeave(to, from, next) {
+      return next(false)
     },
-    {
-        beforeRouteLeave(to, from, next) {
-            return next(false);
-        },
-        name: 'Registration',
-        path: '/registration',
-        component: () => import('../views/RegistrationPage.vue')
+    name: 'Login',
+    path: '/login',
+    component: () => import('../views/LoginPage.vue'),
+  },
+  {
+    beforeRouteLeave(to, from, next) {
+      return next(false)
     },
-    {
-        beforeRouteLeave(to, from, next) {
-            return next(false);
-        },
-        name: 'Forgot',
-        path: '/forgot',
-        component: () => import('../views/ForgotPage.vue')
+    name: 'Registration',
+    path: '/registration',
+    component: () => import('../views/RegistrationPage.vue'),
+  },
+  {
+    beforeRouteLeave(to, from, next) {
+      return next(false)
     },
-    {
-        beforeRouteLeave(to, from, next) {
-            return next(false);
-        },
-        name: 'Default',
-        path: '/home',
-        component: () => import('../views/DefoultPage.vue')
+    name: 'Forgot',
+    path: '/forgot',
+    component: () => import('../views/ForgotPage.vue'),
+  },
+  {
+    beforeRouteLeave(to, from, next) {
+      return next(false)
     },
-    {
-        beforeRouteLeave(to, from, next) {
-            return next(false);
-        },
-        path: '/:catchAll(.*)',
-        component: () => import('../views/404Page.vue')
-    }
-
+    name: 'Default',
+    path: '/home',
+    component: () => import('../views/DefoultPage.vue'),
+  },
+  {
+    beforeRouteLeave(to, from, next) {
+      return next(false)
+    },
+    path: '/:catchAll(.*)',
+    component: () => import('../views/404Page.vue'),
+  },
 ]
 
 const router = createRouter({
-    history: createWebHistory(''),
-    routes,
+  history: createWebHistory(''),
+  routes,
 })
 router.beforeEach(async (to, from) => {
-    if (!!store.state.auth.authenticated &&
-        // ❗️ Avoid an infinite redirect
-        to.name === 'Login') {
-        return {name: 'Home'}
-    }
-    if (
-        // make sure the user is authenticated
-        !store.state.auth.authenticated &&
-        // ❗️ Avoid an infinite redirect
-        to.name !== 'Login' && to.name !== 'Registration' && to.name !== 'Forgot' && to.name !== 'Default'
-    ) {
-        // redirect the user to the login page
-        return {name: 'Default'}
-    }
+  if (
+    !!store.state.auth.authenticated &&
+    // ❗️ Avoid an infinite redirect
+    to.name === 'Login'
+  ) {
+    return { name: 'Home' }
+  }
+  if (
+    // make sure the user is authenticated
+    !store.state.auth.authenticated &&
+    // ❗️ Avoid an infinite redirect
+    to.name !== 'Login' &&
+    to.name !== 'Registration' &&
+    to.name !== 'Forgot' &&
+    to.name !== 'Default'
+  ) {
+    // redirect the user to the login page
+    return { name: 'Default' }
+  }
 })
 
 export default router
-
---
+++ resources/js/views/ComercialTest.vue
@@ -0,0 +1,401 @@
+<template>
+  <v-container>
+    <!-- Line and Area Charts -->
+    <v-row>
+      <v-col cols="12" md="6">
+        <apexchart
+          type="line"
+          :options="lineChartOptions"
+          :series="lineChartSeries"
+        ></apexchart>
+      </v-col>
+      <v-col cols="12" md="6">
+        <apexchart
+          type="area"
+          :options="areaChartOptions"
+          :series="areaChartSeries"
+        ></apexchart>
+      </v-col>
+    </v-row>
+
+    <!-- Bar and Column Charts -->
+    <v-row>
+      <v-col cols="12" md="6">
+        <apexchart
+          type="bar"
+          :options="barChartOptions"
+          :series="barChartSeries"
+        ></apexchart>
+      </v-col>
+      <v-col cols="12" md="6">
+        <apexchart
+          type="column"
+          :options="columnChartOptions"
+          :series="columnChartSeries"
+        ></apexchart>
+      </v-col>
+    </v-row>
+
+    <!-- Pie and Donut Charts -->
+    <v-row>
+      <v-col cols="12" md="6">
+        <apexchart
+          type="pie"
+          :options="pieChartOptions"
+          :series="pieChartSeries"
+        ></apexchart>
+      </v-col>
+      <v-col cols="12" md="6">
+        <apexchart
+          type="donut"
+          :options="donutChartOptions"
+          :series="donutChartSeries"
+        ></apexchart>
+      </v-col>
+    </v-row>
+
+    <!-- Radar and Polar Area Charts -->
+    <v-row>
+      <v-col cols="12" md="6">
+        <apexchart
+          type="radar"
+          :options="radarChartOptions"
+          :series="radarChartSeries"
+        ></apexchart>
+      </v-col>
+      <v-col cols="12" md="6">
+        <apexchart
+          type="polarArea"
+          :options="polarAreaChartOptions"
+          :series="polarAreaChartSeries"
+        ></apexchart>
+      </v-col>
+    </v-row>
+
+    <!-- Scatter and Bubble Charts -->
+    <v-row>
+      <v-col cols="12" md="6">
+        <apexchart
+          type="scatter"
+          :options="scatterChartOptions"
+          :series="scatterChartSeries"
+        ></apexchart>
+      </v-col>
+      <v-col cols="12" md="6">
+        <apexchart
+          type="bubble"
+          :options="bubbleChartOptions"
+          :series="bubbleChartSeries"
+        ></apexchart>
+      </v-col>
+    </v-row>
+
+    <!-- Heatmap and Treemap Charts -->
+    <v-row>
+      <v-col cols="12" md="6">
+        <apexchart
+          type="heatmap"
+          :options="heatmapChartOptions"
+          :series="heatmapChartSeries"
+        ></apexchart>
+      </v-col>
+      <v-col cols="12" md="6">
+        <apexchart
+          type="treemap"
+          :options="treemapChartOptions"
+          :series="treemapChartSeries"
+        ></apexchart>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import VueApexCharts from 'vue3-apexcharts'
+
+export default {
+  name: 'PageTest',
+  components: {
+    apexchart: VueApexCharts,
+  },
+  data() {
+    return {
+      // Line Chart
+      lineChartOptions: {
+        chart: {
+          type: 'line',
+        },
+        xaxis: {
+          categories: [
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+            'Jul',
+            'Aug',
+            'Sep',
+          ],
+        },
+      },
+      lineChartSeries: [
+        {
+          name: 'Series 1',
+          data: [30, 40, 35, 50, 49, 60, 70, 91, 125],
+        },
+      ],
+      // Area Chart
+      areaChartOptions: {
+        chart: {
+          type: 'area',
+        },
+        xaxis: {
+          categories: [
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+            'Jul',
+            'Aug',
+            'Sep',
+          ],
+        },
+      },
+      areaChartSeries: [
+        {
+          name: 'Series 1',
+          data: [30, 40, 35, 50, 49, 60, 70, 91, 125],
+        },
+      ],
+      // Bar Chart
+      barChartOptions: {
+        chart: {
+          type: 'bar',
+        },
+        xaxis: {
+          categories: [
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+            'Jul',
+            'Aug',
+            'Sep',
+          ],
+        },
+      },
+      barChartSeries: [
+        {
+          name: 'Series 1',
+          data: [30, 40, 35, 50, 49, 60, 70, 91, 125],
+        },
+      ],
+      // Column Chart
+      columnChartOptions: {
+        chart: {
+          type: 'column',
+        },
+        xaxis: {
+          categories: [
+            'Jan',
+            'Feb',
+            'Mar',
+            'Apr',
+            'May',
+            'Jun',
+            'Jul',
+            'Aug',
+            'Sep',
+          ],
+        },
+      },
+      columnChartSeries: [
+        {
+          name: 'Series 1',
+          data: [30, 40, 35, 50, 49, 60, 70, 91, 125],
+        },
+      ],
+      // Pie Chart
+      pieChartOptions: {
+        chart: {
+          type: 'pie',
+        },
+      },
+      pieChartSeries: [44, 55, 13, 43, 22],
+      // Donut Chart
+      donutChartOptions: {
+        chart: {
+          type: 'donut',
+        },
+      },
+      donutChartSeries: [44, 55, 13, 43, 22],
+      // Radar Chart
+      radarChartOptions: {
+        chart: {
+          type: 'radar',
+        },
+      },
+      radarChartSeries: [
+        {
+          name: 'Series 1',
+          data: [80, 50, 30, 40, 100],
+        },
+      ],
+      // Polar Area Chart
+      polarAreaChartOptions: {
+        chart: {
+          type: 'polarArea',
+        },
+      },
+      polarAreaChartSeries: [30, 40, 45, 50, 49],
+      // Scatter Chart
+      scatterChartOptions: {
+        chart: {
+          type: 'scatter',
+        },
+      },
+      scatterChartSeries: [
+        {
+          name: 'Series 1',
+          data: [
+            [10, 20],
+            [15, 25],
+            [20, 30],
+            [25, 35],
+            [30, 40],
+          ],
+        },
+      ],
+      // Bubble Chart
+      bubbleChartOptions: {
+        chart: {
+          type: 'bubble',
+        },
+      },
+      bubbleChartSeries: [
+        {
+          name: 'Series 1',
+          data: [
+            {
+              x: 10,
+              y: 20,
+              z: 30,
+            },
+            {
+              x: 15,
+              y: 25,
+              z: 35,
+            },
+            {
+              x: 20,
+              y: 30,
+              z: 40,
+            },
+            {
+              x: 25,
+              y: 35,
+              z: 45,
+            },
+            {
+              x: 30,
+              y: 40,
+              z: 50,
+            },
+          ],
+        },
+      ],
+      // Heatmap Chart
+      heatmapChartOptions: {
+        chart: {
+          type: 'heatmap',
+        },
+      },
+      heatmapChartSeries: [
+        {
+          name: 'Series 1',
+          data: [
+            {
+              x: 'Monday',
+              y: 0,
+              value: 4,
+            },
+            {
+              x: 'Tuesday',
+              y: 1,
+              value: 2,
+            },
+            {
+              x: 'Wednesday',
+              y: 2,
+              value: 6,
+            },
+            {
+              x: 'Thursday',
+              y: 3,
+              value: 8,
+            },
+            {
+              x: 'Friday',
+              y: 4,
+              value: 10,
+            },
+            {
+              x: 'Saturday',
+              y: 5,
+              value: 12,
+            },
+            {
+              x: 'Sunday',
+              y: 6,
+              value: 14,
+            },
+          ],
+        },
+      ],
+      // Treemap Chart
+      treemapChartOptions: {
+        chart: {
+          type: 'treemap',
+        },
+      },
+      treemapChartSeries: [
+        {
+          data: [
+            {
+              x: 'North America',
+              y: 500,
+            },
+            {
+              x: 'Europe',
+              y: 300,
+            },
+            {
+              x: 'Asia',
+              y: 400,
+            },
+            {
+              x: 'Africa',
+              y: 100,
+            },
+            {
+              x: 'South America',
+              y: 200,
+            },
+          ],
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped>
+h1 {
+  font-size: 2em;
+}
+</style>
