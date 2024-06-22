1099e8428190df2ac3210c511a6296e56860ee66
vindexTOS <giorgikutateladze1998@gmail.com>
2024-06-22 23:19:23 +0400

    config change

--- app/Services/MqttConnectionService.php
+++ app/Services/MqttConnectionService.php
@@ -22,11 +22,11 @@ class MqttConnectionService {
     // Constructor is private
     private function __construct() {
 
-        $this->server = config('3.71.18.216');
-        $this->port = config('1883');
+        $this->server = '3.71.18.216';
+        $this->port = '1883';
         $this->clientId = rand(5, 15);
-        $this->username = config('username');
-        $this->password = config('12345678');
+        $this->username = 'username';
+        $this->password ='12345678';
         $this->mqtt_version = MqttClient::MQTT_3_1_1;
     }
 
