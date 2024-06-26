a69bce007e5ce17c680abc7d4154e9e3447e79e7
vindexTOS <giorgikutateladze1998@gmail.com>
2024-06-26 12:20:47 +0400

    added exel to FixedCard branch

--- app/Console/Commands/UserSubscriptionCheck.php
+++ app/Console/Commands/UserSubscriptionCheck.php
@@ -33,14 +33,21 @@ class UserSubscriptionCheck extends Command
         $devices = Device::where('pay_day', $today)
         ->where('op_mode', 0)
         ->get();
+        
+        Log::debug("SHemovedi subsshi");
+        
         foreach ($devices as $device) {
+            
             $deviceEarning = 0;
             $users = $device->users; // Assuming DeviceUser is the related model name, and 'users' is the relationship method name in Device model.
+            Log::debug("pirveli loop");
             
             foreach ($users as $user) {
                 $userFixedBalnce = $user->fixed_card_amount;
                 $userCardAmount = Card::where('user_id', $user->id)->count();
-                $fixedCard = $userFixedBalnce * $userCardAmount;
+                $fixedCard = $userFixedBalnce * $userCardAmount * 100;
+                Log::debug($fixedCard);
+                
                 $subscriptionDate = $user->pivot->subscription
                 ? Carbon::parse($user->pivot->subscription)
                 : null;
@@ -48,56 +55,46 @@ class UserSubscriptionCheck extends Command
                 ->addMonth()
                 ->startOfMonth()
                 ->addDays($device->pay_day - 1);
-                if (
-                    $user->balance >= $device->tariff_amount + $fixedCard &&
-                    $user->freezed_balance >= $device->tariff_amount &&
-                    !is_null($subscriptionDate) &&
-                    $subscriptionDate->lt($nextMonthPayDay)
-                    ) {
-                        // Start transaction
-                        DB::beginTransaction();
-                        try {
-                            // Update User balances
+                // როცა დევაისის ტარიფი უდრის ნულს
+                
+                
+                if (false) {
+                    $userBalance = $user->balance;
+                    
+                    
+                    if (
+                        $userBalance >= $fixedCard &&
+                        !is_null($subscriptionDate) &&
+                        $subscriptionDate->lt($nextMonthPayDay)
+                        ) {
+                            DB::beginTransaction();
                             
-                            $user->balance -= $device->tariff_amount + $fixedCard;
-                            
-                            $user->freezed_balance -= $device->tariff_amount;
-                            
-                            $deviceTariffWithCardBalance =
-                            $device->tariff_amount + $fixedCard;
-                            
-                            if (
-                                $user->balance - $user->freezed_balance >=
-                                $deviceTariffWithCardBalance
-                                ) {
-                                    $currentDay = Carbon::now()->day;
-                                    
-                                    if ($currentDay < $device->pay_day) {
-                                        $nextMonthPayDay = Carbon::now()
-                                        ->startOfMonth()
-                                        ->addDays($device->pay_day - 1);
-                                    } else {
-                                        $nextMonthPayDay = Carbon::now()
-                                        ->addMonth()
-                                        ->startOfMonth()
-                                        ->addDays($device->pay_day - 1);
-                                    }
-                                    DeviceUser::where('device_id', $device->id)
-                                    ->where('user_id', $user->id)
-                                    ->update(['subscription' => $nextMonthPayDay]);
-                                    
-                                    $user->freezed_balance =
-                                    $user->freezed_balance + $device->tariff_amount;
+                            try {
+                                $user->balance -= $fixedCard  ;
+                                
+                                
+                                $currentDay = Carbon::now()->day;
+                                
+                                if ($currentDay < $device->pay_day) {
+                                    $nextMonthPayDay = Carbon::now()
+                                    ->startOfMonth()
+                                    ->addDays($device->pay_day - 1);
+                                } else {
+                                    $nextMonthPayDay = Carbon::now()
+                                    ->addMonth()
+                                    ->startOfMonth()
+                                    ->addDays($device->pay_day - 1);
                                 }
+                                DeviceUser::where('device_id', $device->id)
+                                ->where('user_id', $user->id)
+                                ->update([
+                                    'subscription' => $nextMonthPayDay,
+                                ]);
                                 
                                 $user->save();
-                                
-                                // Update device earnings
-                                $deviceEarning += $deviceTariffWithCardBalance;
-                                // Commit the transaction
+                                $deviceEarning += $fixedCard;
                                 DB::commit();
                             } catch (\Exception $e) {
-                                // An error occurred; rollback the transaction
                                 DB::rollback();
                                 $this->error(
                                     'Failed to process user ' .
@@ -109,51 +106,136 @@ class UserSubscriptionCheck extends Command
                                 );
                             }
                         }
+                        
+                        //  როცა დვაისის ტარიფი ნოლზე მეტია
+                    } else {
+                        if (
+                            $user->balance >= $device->tariff_amount + $fixedCard    &&
+                            $user->freezed_balance >= $device->tariff_amount &&
+                            !is_null($subscriptionDate) &&
+                            $subscriptionDate->lt($nextMonthPayDay)
+                            ) {
+                                // Start transaction
+                                DB::beginTransaction();
+                                try {
+                                    // Update User balances
+                                    
+                                    $user->balance -=
+                                    $device->tariff_amount + $fixedCard;
+                                    
+                                    $user->freezed_balance -= $device->tariff_amount;
+                                    
+                                    $deviceTariffWithCardBalance =
+                                    $device->tariff_amount + $fixedCard;
+                                    
+                                    if (
+                                        $user->balance - $user->freezed_balance >=
+                                        $deviceTariffWithCardBalance
+                                        ) {
+                                            $currentDay = Carbon::now()->day;
+                                            
+                                            if ($currentDay < $device->pay_day) {
+                                                $nextMonthPayDay = Carbon::now()
+                                                ->startOfMonth()
+                                                ->addDays($device->pay_day - 1);
+                                            } else {
+                                                $nextMonthPayDay = Carbon::now()
+                                                ->addMonth()
+                                                ->startOfMonth()
+                                                ->addDays($device->pay_day - 1);
+                                            }
+                                            DeviceUser::where('device_id', $device->id)
+                                            ->where('user_id', $user->id)
+                                            ->update([
+                                                'subscription' => $nextMonthPayDay,
+                                            ]);
+                                            
+                                            $user->freezed_balance =
+                                            $user->freezed_balance +
+                                            $device->tariff_amount;
+                                        }
+                                        
+                                        $user->save();
+                                        
+                                        // Update device earnings
+                                        $deviceEarning += $deviceTariffWithCardBalance;
+                                        // Commit the transaction
+                                        DB::commit();
+                                    } catch (\Exception $e) {
+                                        // An error occurred; rollback the transaction
+                                        DB::rollback();
+                                        $this->error(
+                                            'Failed to process user ' .
+                                            $user->id .
+                                            ' with device ' .
+                                            $device->id .
+                                            ': ' .
+                                            $e->getMessage()
+                                        );
+                                    }
+                                }
+                            }
+                        }
+                        
+                        $user = User::where('id', $device->users_id)->first();
+                        if ($user->cashback == 0) {
+                            $user = User::where('id', $device->company_id)->first();
+                        }
+                        $device = Device::where('id', $device->id)->first();
+                        $deviceEarn = DeviceEarn::where('device_id', $device->id)
+                        ->where('month', $currentMonth)
+                        ->where('year', $currentYear)
+                        ->first();
+                        if (empty($deviceEarn)) {
+                            if ($user && $device) {
+                                if( $device->deviceTariffAmount !== null           ){
+                                    DeviceEarn::create([
+                                        'device_id' => $device->id,
+                                        'month' => $currentMonth,
+                                        'year' => $currentYear,
+                                        'earnings' => $deviceEarning,
+                                        'cashback' => $user->cashback,
+                                        'deviceTariff' => $device->deviceTariffAmount,
+                                    ]);
+                                }else{
+                                    DeviceEarn::create([
+                                        'device_id' => $device->id,
+                                        'month' => $currentMonth,
+                                        'year' => $currentYear,
+                                        'earnings' => $deviceEarning,
+                                        'cashback' => $user->cashback,
+                                        'deviceTariff' => 0,
+                                    ]);
+                                }
+                                
+                            } else {
+                                DeviceEarn::create([
+                                    'device_id' => $device->id,
+                                    'month' => $currentMonth,
+                                    'year' => $currentYear,
+                                    'earnings' => $deviceEarning,
+                                ]);
+                            }
+                        } else {
+                            if ($user && $device) {
+                                if ($device->deviceTariffAmount !== null) {
+                                    $deviceEarn->earnings += $deviceEarning;
+                                    $deviceEarn->cashback = $user->cashback;
+                                    $deviceEarn->deviceTariff = $device->deviceTariffAmount;
+                                    $deviceEarn->save();
+                                }
+                                $deviceEarn->earnings += $deviceEarning;
+                                $deviceEarn->cashback = $user->cashback;
+                                $deviceEarn->save();
+                            } else {
+                                $deviceEarn->earnings += $deviceEarning;
+                                $deviceEarn->save();
+                            }
+                        }
                     }
                     
-                    $user = User::where('id', $device->users_id)->first();
-                    if ($user->cashback == 0) {
-                        $user = User::where('id', $device->company_id)->first();
-                    }
-                    $device = Device::where('id', $device->id)->first();
-                    $deviceEarn = DeviceEarn::where('device_id', $device->id)
-                    ->where('month', $currentMonth)
-                    ->where('year', $currentYear)
-                    ->first();
-                    if (empty($deviceEarn)) {
-                        if ($user && $device) {
-                            DeviceEarn::create([
-                                'device_id' => $device->id,
-                                'month' => $currentMonth,
-                                'year' => $currentYear,
-                                'earnings' => $deviceEarning,
-                                'cashback' => $user->cashback,
-                                'deviceTariff' => $device->deviceTariffAmount,
-                            ]);
-                        } else {
-                            DeviceEarn::create([
-                                'device_id' => $device->id,
-                                'month' => $currentMonth,
-                                'year' => $currentYear,
-                                'earnings' => $deviceEarning,
-                            ]);
-                        }
-                    } else {
-                        if ($user && $device) {
-                            $deviceEarn->earnings += $deviceEarning;
-                            $deviceEarn->cashback = $user->cashback;
-                            $deviceEarn->deviceTariff = $device->deviceTariffAmount;
-                            $deviceEarn->save();
-                        } else {
-                            $deviceEarn->earnings += $deviceEarning;
-                            $deviceEarn->save();
-                        }
-                    }
+                    $this->info(
+                        'User subscriptions have been checked and updated successfully.'
+                    );
                 }
-                
-                $this->info(
-                    'User subscriptions have been checked and updated successfully.'
-                );
             }
-        }
-        
