a21fd0c8f9bbcf2daea03f4d94d17f0174555d95
vindexTOS <giorgikutateladze1998@gmail.com>
2024-07-04 17:08:50 +0400

    re added deletation

--- app/Http/Controllers/API/CardController.php
+++ app/Http/Controllers/API/CardController.php
@@ -31,20 +31,20 @@ class CardController extends Controller
     public function store(Request $request)
     {
         $data = DB::table('activation_codes')
-        ->where('code', $request->card_number)
-        ->first();
+            ->where('code', $request->card_number)
+            ->first();
         if (empty($data)) {
             return response()->json(['message' => 'incorrect_code'], 422);
         }
 
         $device = Device::where('id', $data->device_id)->first();
         $userCards = Card::where('device_id', $data->device_id)
-        ->where('user_id', Auth::id())
-        ->pluck('user_id')
-        ->toArray();
+            ->where('user_id', Auth::id())
+            ->pluck('user_id')
+            ->toArray();
         $relate = DeviceUser::where('user_id', Auth::id())
-        ->where('device_id', $data->device_id)
-        ->first();
+            ->where('device_id', $data->device_id)
+            ->first();
         //        if(empty($relate)) {
         //            DeviceUser::create([
         //                'user_id' => Auth::id(),
@@ -79,20 +79,20 @@ class CardController extends Controller
     public function storeForUser(Request $request)
     {
         $data = DB::table('activation_codes')
-        ->where('code', $request->card_number)
-        ->first();
+            ->where('code', $request->card_number)
+            ->first();
         if (empty($data)) {
             return response()->json(['message' => 'incorrect_code'], 422);
         }
 
         $device = Device::where('id', $data->device_id)->first();
         $userCards = Card::where('device_id', $data->device_id)
-        ->where('user_id', $request->user_id)
-        ->pluck('user_id')
-        ->toArray();
+            ->where('user_id', $request->user_id)
+            ->pluck('user_id')
+            ->toArray();
         $relate = DeviceUser::where('user_id', $request->user_id)
-        ->where('device_id', $data->device_id)
-        ->first();
+            ->where('device_id', $data->device_id)
+            ->first();
 
         if ($device->limit > count($userCards) && !empty($relate)) {
             Card::create([
@@ -130,18 +130,18 @@ class CardController extends Controller
         $device = Device::where('id', $request->device_id)->first();
         $Balance = Auth::user()['balance'];
         $data = DB::table('elevator_codes')
-        ->where('user_id', Auth::id())
-        ->where('device_id', $request->device_id)
-        ->where('expires_at', '>', Carbon::now())
-        ->first();
+            ->where('user_id', Auth::id())
+            ->where('device_id', $request->device_id)
+            ->where('expires_at', '>', Carbon::now())
+            ->first();
         if (!empty($data)) {
             return $data->code;
         }
         $canCode = false;
         if ($device->op_mode == 0) {
             $deviceUser = DeviceUser::where('device_id', $device->id)
-            ->where('user_id', Auth::id())
-            ->first();
+                ->where('user_id', Auth::id())
+                ->first();
             $subscriptionDate = Carbon::parse($deviceUser->subscription);
             $now = Carbon::now();
             if ($subscriptionDate->gt($now)) {
@@ -175,15 +175,15 @@ class CardController extends Controller
         $canCode = false;
         if ($device->op_mode == 0) {
             $deviceIds = Device::where('users_id', $device->users_id)
-            ->pluck('id')
-            ->toArray();
+                ->pluck('id')
+                ->toArray();
 
             $card = Card::where('user_id', $user->id)
-            ->whereIn('device_id', $deviceIds)
-            ->first();
+                ->whereIn('device_id', $deviceIds)
+                ->first();
             $deviceUser = DeviceUser::where('device_id', $card->device_id)
-            ->where('user_id', Auth::id())
-            ->first();
+                ->where('user_id', Auth::id())
+                ->first();
             $subscriptionDate = Carbon::parse($deviceUser->subscription);
             $now = Carbon::now();
             if ($subscriptionDate->gt($now)) {
@@ -203,8 +203,8 @@ class CardController extends Controller
         }
         if ($canCode) {
             $lastAmount = LastUserAmount::where('user_id', $user->id)
-            ->where('device_id', $device->id)
-            ->first();
+                ->where('device_id', $device->id)
+                ->first();
 
             if (empty($lastAmount->user_id)) {
                 LastUserAmount::insert([
@@ -214,7 +214,7 @@ class CardController extends Controller
                 ]);
             } else {
                 $lastAmount->last_amount =
-                $user->balance - $user->freezed_balance;
+                    $user->balance - $user->freezed_balance;
                 $lastAmount->save();
             }
             $payload = $this->generateHexPayload(1, [
@@ -238,185 +238,180 @@ class CardController extends Controller
                     $lastAmountCurrentDevice = LastUserAmount::where(
                         'user_id',
                         $user->id
-                        )
+                    )
                         ->where('device_id', $value2->id)
                         ->first();
 
-                        if (empty($lastAmountCurrentDevice->user_id)) {
-                            LastUserAmount::insert([
-                                'user_id' => $user->id,
-                                'device_id' => $value2->id,
-                                'last_amount' =>
-                                $user->balance - $user->freezed_balance,
-                            ]);
-                        } else {
-                            $lastAmountCurrentDevice->last_amount =
-                            $user->balance - $user->freezed_balance;
-                            $lastAmountCurrentDevice->save();
-                        }
-                        $payload = $this->generateHexPayload(5, [
-                            [
-                                'type' => 'string',
-                                'value' => str_pad($user->id, 6, '0', STR_PAD_LEFT),
-                            ],
-                            [
-                                'type' => 'number',
-                                'value' => 0,
-                            ],
-                            [
-                                'type' => 'number16',
-                                'value' => $user->balance - $user->freezed_balance,
-                            ],
+                    if (empty($lastAmountCurrentDevice->user_id)) {
+                        LastUserAmount::insert([
+                            'user_id' => $user->id,
+                            'device_id' => $value2->id,
+                            'last_amount' =>
+                            $user->balance - $user->freezed_balance,
                         ]);
-                        $this->publishMessage($value2->dev_id, $payload);
+                    } else {
+                        $lastAmountCurrentDevice->last_amount =
+                            $user->balance - $user->freezed_balance;
+                        $lastAmountCurrentDevice->save();
                     }
+                    $payload = $this->generateHexPayload(5, [
+                        [
+                            'type' => 'string',
+                            'value' => str_pad($user->id, 6, '0', STR_PAD_LEFT),
+                        ],
+                        [
+                            'type' => 'number',
+                            'value' => 0,
+                        ],
+                        [
+                            'type' => 'number16',
+                            'value' => $user->balance - $user->freezed_balance,
+                        ],
+                    ]);
+                    $this->publishMessage($value2->dev_id, $payload);
                 }
-            } else {
-                return response()->json(
-                    ['message' => 'გთხოვთ შეავსოთ ბალანსი'],
-                    422
-                );
             }
+        } else {
+            return response()->json(
+                ['message' => 'გთხოვთ შეავსოთ ბალანსი'],
+                422
+            );
         }
-        public function saveOrUpdateEarnings($deviceId, $earningsValue, $companyId)
-        {
-            // Generate the date for month_year
-            // TO DO find company cashback and add  to DeviceEarn find device tariff with deviceID
+    }
+    public function saveOrUpdateEarnings($deviceId, $earningsValue, $companyId)
+    {
+        // Generate the date for month_year
+        // TO DO find company cashback and add  to DeviceEarn find device tariff with deviceID
 
-            $now = Carbon::now();
-            $user = User::where('id', $companyId)->first();
-            $device = Device::where('id', $deviceId)->first();
-            // Try to retrieve the entry for the given device and month_year
-            $deviceEarnings = DeviceEarn::where('device_id', $deviceId)
+        $now = Carbon::now();
+        $user = User::where('id', $companyId)->first();
+        $device = Device::where('id', $deviceId)->first();
+        // Try to retrieve the entry for the given device and month_year
+        $deviceEarnings = DeviceEarn::where('device_id', $deviceId)
             ->where('month', $now->month)
             ->where('year', $now->year)
             ->first();
-            if (!empty($deviceEarnings)) {
-                if ($user && $device) {
-                    $deviceEarnings->earnings += $earningsValue;
-                    $deviceEarnings->cashback = $user->cashback;
-                    $deviceEarnings->deviceTariff = $device->deviceTariffAmount;
-                    $deviceEarnings->save();
-                } else {
-                    $deviceEarnings->earnings += $earningsValue;
-
-                    $deviceEarnings->save();
-                }
+        if (!empty($deviceEarnings)) {
+            if ($user && $device) {
+                $deviceEarnings->earnings += $earningsValue;
+                $deviceEarnings->cashback = $user->cashback;
+                $deviceEarnings->deviceTariff = $device->deviceTariffAmount;
+                $deviceEarnings->save();
             } else {
-                if ($user && $device) {
-                    DeviceEarn::create([
-                        'company_id' => $companyId,
-                        'device_id' => $deviceId,
-                        'month' => $now->month,
-                        'year' => $now->year,
-                        'earnings' => $earningsValue,
-                        'cashback' => $user->cashback,
-                        'deviceTariff' => $device->deviceTariffAmount,
-                    ]);
-                } else {
-                    DeviceEarn::create([
-                        'company_id' => $companyId,
-                        'device_id' => $deviceId,
-                        'month' => $now->month,
-                        'year' => $now->year,
-                        'earnings' => $earningsValue,
-                    ]);
-                }
+                $deviceEarnings->earnings += $earningsValue;
+
+                $deviceEarnings->save();
             }
-            // Save the model (either updates or creates based on existence)
-        }
-
-        public function generateHexPayload($command, $payload)
-        {
-            return [
-                'command' => $command,
-                'payload' => $payload,
-            ];
-        }
-        public function publishMessage($device_id, $payload)
-        {
-            $data = [
-                'device_id' => $device_id,
-                'payload' => $payload,
-            ];
-
-            Log::debug($payload);
-            $queryParams = http_build_query($data);
-            $response = Http::get('http://localhost:3000/mqtt/general?' . $queryParams);
-            $responseData = $response->json();
-            
-            Log::debug("HTTP Response body: " . $responseData);
-
-            
-            $responseJson = $response->json();
-            Log::debug("HTTP Response JSON: " . json_encode($responseJson));
-
-            // Check the structure of the parsed response
-            // if (is_array($responseJson)) {
-            //     foreach ($responseJson as $key => $value) {
-            //         Log::debug("Response JSON Key: " . $key . ", Value Type: " . gettype($value));
-            //     }
-            // } else {
-            //     Log::error("Response is not an array");
-            // }
-
-            return ["data" =>"ata"];
-        }
-
-
-
-        public function update(Request $request, Card $card)
-        {
-            $card->update($request->all());
-            return response()->json($card, 200);
-        }
-
-        public function destroy(Card $card)
-        {
-
-
-            // Publish the message using MQTT
-            try {
-
-
-                $command = 7;  // Command 7 in hexadecimal
-
-                // Generate the payload
-            
-                $device = Device::where('id', $card->device_id)->first();
-              
-                $payload = $this->generateHexPayload($command, [
-                    [
-                        'type' => 'string',
-                        'value' => str_pad($card->card_number, 8, '0', STR_PAD_RIGHT)  ,
-                    ],    [
-                        'type' => 'number',
-                        'value' => 0,
-                    ],
-                ]);        
-              
-                // Log::debug("Generated payload: " . $payload);
-
-                $response = $this->publishMessage($device->dev_id, $payload);
-                Log::debug("Response from MQTT server: " . json_encode($response));
-                // $card->delete();
-              
-                if (isset($response['command']) && isset($response['payload'])) {
-                    Log::debug("Command: " . $response['command']);
-                    Log::debug("Payload: " . json_encode($response['payload']));
-                } else {
-                    Log::error("Unexpected response structure: " . json_encode($response));
-                }
-            } catch (\Exception $e) {
-                Log::error("Error publishing message: " . $e->getMessage());
-                return response()->json(['error' => 'Failed to publish message'], 500);
+        } else {
+            if ($user && $device) {
+                DeviceEarn::create([
+                    'company_id' => $companyId,
+                    'device_id' => $deviceId,
+                    'month' => $now->month,
+                    'year' => $now->year,
+                    'earnings' => $earningsValue,
+                    'cashback' => $user->cashback,
+                    'deviceTariff' => $device->deviceTariffAmount,
+                ]);
+            } else {
+                DeviceEarn::create([
+                    'company_id' => $companyId,
+                    'device_id' => $deviceId,
+                    'month' => $now->month,
+                    'year' => $now->year,
+                    'earnings' => $earningsValue,
+                ]);
             }
+        }
+        // Save the model (either updates or creates based on existence)
+    }
 
-            return response()->json("Card Has Been Deleted", 204);
+    public function generateHexPayload($command, $payload)
+    {
+        return [
+            'command' => $command,
+            'payload' => $payload,
+        ];
+    }
+    public function publishMessage($device_id, $payload)
+    {
+        $data = [
+            'device_id' => $device_id,
+            'payload' => $payload,
+        ];
+
+        Log::debug($payload);
+        $queryParams = http_build_query($data);
+        $response = Http::get('http://localhost:3000/mqtt/general?' . $queryParams);
+        $responseData = $response->json();
+
+        Log::debug("HTTP Response body: " . $responseData);
+
+
+        $responseJson = $response->json();
+        Log::debug("HTTP Response JSON: " . json_encode($responseJson));
+
+        // Check the structure of the parsed response
+        // if (is_array($responseJson)) {
+        //     foreach ($responseJson as $key => $value) {
+        //         Log::debug("Response JSON Key: " . $key . ", Value Type: " . gettype($value));
+        //     }
+        // } else {
+        //     Log::error("Response is not an array");
+        // }
+
+        return ["data" => "ata"];
+    }
+
+
+
+    public function update(Request $request, Card $card)
+    {
+        $card->update($request->all());
+        return response()->json($card, 200);
+    }
+
+    public function destroy(Card $card)
+    {
+
+
+        // Publish the message using MQTT
+        try {
+
+
+            $command = 7;  // Command 7 in hexadecimal
+
+            // Generate the payload
+
+            $device = Device::where('id', $card->device_id)->first();
+
+            $payload = $this->generateHexPayload($command, [
+                [
+                    'type' => 'string',
+                    'value' => str_pad($card->card_number, 8, '0', STR_PAD_RIGHT),
+                ], [
+                    'type' => 'number',
+                    'value' => 0,
+                ],
+            ]);
+
+            // Log::debug("Generated payload: " . $payload);
+
+            $response = $this->publishMessage($device->dev_id, $payload);
+            Log::debug("Response from MQTT server: " . json_encode($response));
+            $card->delete();
+
+            if (isset($response['command']) && isset($response['payload'])) {
+                Log::debug("Command: " . $response['command']);
+                Log::debug("Payload: " . json_encode($response['payload']));
+            } else {
+                Log::error("Unexpected response structure: " . json_encode($response));
+            }
+        } catch (\Exception $e) {
+            Log::error("Error publishing message: " . $e->getMessage());
+            return response()->json(['error' => 'Failed to publish message'], 500);
         }
 
-
-
-
-
-        }
+        return response()->json("Card Has Been Deleted", 204);
+    }
+}
