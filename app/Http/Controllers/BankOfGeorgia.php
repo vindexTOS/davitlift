81e86527a17755ef1b9db4edf5fa5df2284091d7
vindexTOS <giorgikutateladze1998@gmail.com>
2024-08-30 18:38:45 +0400

    return the back

--- app/Http/Controllers/BankOfGeorgia.php
+++ app/Http/Controllers/BankOfGeorgia.php
@@ -350,7 +350,7 @@ private function VerifyUser(Request $request)
 
         Tbctransaction::create([
             'user_id' => $userId,
-            'amount' =>   $amount  ,
+            'amount' =>  number_format($amount / 100, 2),
             'FileId' => $FileId,
             'order_id' => $order_id,
             'type' => $type,
