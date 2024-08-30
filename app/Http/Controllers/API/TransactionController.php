56cd4969c400eac43f63c4908e41a94703e3f55a
vindexTOS <giorgikutateladze1998@gmail.com>
2024-08-30 18:14:59 +0400

    s

--- app/Http/Controllers/API/TransactionController.php
+++ app/Http/Controllers/API/TransactionController.php
@@ -344,7 +344,8 @@ class TransactionController extends Controller
                         $FileId,
                         $type
                         ) {
-                             Tbctransaction::create([
+                            Log::debug($type);
+                            Tbctransaction::create([
                                 'user_id' => $userId,
                                 'amount' => $amount,
                                 'FileId' => $FileId,
@@ -439,9 +440,7 @@ class TransactionController extends Controller
                                 $transaction->save();
                                 return $data;
                             }
-                             public function testTransactionOrderForEcom(){
-                                 
-                             }
+                            
                             public function makeOrderTransaction($amount, $user, $token)
                             {
                                 $data = [];
@@ -536,7 +535,8 @@ class TransactionController extends Controller
                                             public function updateUserData($data, $transaction, $order_id, $isFastPay)
                                                 {
                                                 try {
-                                                     
+                                                    Log::debug('აფდეითში შემოსვლა');
+                                                    
                                                     $user = User::where('id', $transaction->user_id)
                                                     ->with('devices')
                                                     ->first();
@@ -547,17 +547,14 @@ class TransactionController extends Controller
                                                         
                                                         $sakomisio = number_format($sakomisio, 2, '.', '');
                                                     }
-
-                                               
                                                     $user->balance =
                                                     intval($user->balance) + $transfer_amount - $sakomisio;
                                                     $userCardAmount = Card::where('user_id', $user->id)->count();
-                                                    Log::debug( $user->balance);
                                                     
-
                                                     foreach ($user->devices as $key => $device) {
                                                         if ($device->op_mode === '0') {
-                                                             
+                                                            Log::debug('op_mode = 0');
+                                                            
                                                             $subscriptionDate = $device->pivot->subscription
                                                             ? Carbon::parse($device->pivot->subscription)
                                                             : null;
@@ -567,19 +564,22 @@ class TransactionController extends Controller
                                                                 
                                                                 ->startOfMonth()
                                                                 ->addDays($device->pay_day - 1);
-                                                             } else {
+                                                                Log::debug('შემდეგი თარიღი>>> 1' . $nextMonthPayDay);
+                                                            } else {
                                                                 $nextMonthPayDay = Carbon::now()
                                                                 ->addMonth()
                                                                 ->startOfMonth()
                                                                 ->addDays($device->pay_day - 1);
                                                                 
-                                                             }
+                                                                Log::debug('შემდეგი თარიღი>>> 2' . $nextMonthPayDay);
+                                                            }
                                                             if (
                                                                 is_null($subscriptionDate) ||
                                                                 ($subscriptionDate &&
                                                                 $subscriptionDate->lt($nextMonthPayDay))
                                                                 ) {
-                                                                     
+                                                                    Log::debug('is_null');
+                                                                    
                                                                     $cardAmount =
                                                                     $userCardAmount * $user->fixed_card_amount;
                                                                     if (
