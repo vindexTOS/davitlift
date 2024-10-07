5c80c84f5a9bcb2c7d49a9b869412642f28970d5
vindexTOS <giorgikutateladze1998@gmail.com>
2024-10-07 22:36:14 +0400

    no money console out

--- app/Http/Controllers/MqttController.php
+++ app/Http/Controllers/MqttController.php
@@ -274,54 +274,9 @@ class MqttController extends Controller
 
             }
             }else if ($device->op_mode == "1"){
-                // $this->handleOpMode($device->op_mode, $user, $device, $data);
-                $lastAmount = LastUserAmount::where('user_id', $user->id)
-                ->where('device_id', $device->id)
-                ->first();
-
-            if (empty($lastAmount->user_id)) {
-                LastUserAmount::insert([
-                    'user_id' => $user->id,
-                    'device_id' => $device->id,
-                    'last_amount' =>
-                    $user->balance - $device->tariff_amount,
-                ]);
-            } else {
-                $lastAmount->last_amount =
-                    $user->balance - $device->tariff_amount;
-                $lastAmount->save();
-            }
-            $payload = $this->generateHexPayload(3, [
-                [
-                    'type' => 'string',
-                    'value' => str_pad($user->id, 6, '0', STR_PAD_LEFT),
-                ],
-                [
-                    'type' => 'number',
-                    'value' => 0,
-                ],
-                ['type' => 'string', 'value' => $data['payload']],
-                [
-                    'type' => 'number',
-                    'value' => 0,
-                ],
-                [
-                    'type' => 'number16',
-                    'value' => $user->balance - $device->tariff_amount,
-                ],
-            ]);
-            $user->save();
-            $this->saveOrUpdateEarnings(
-                $device->id,
-                $device->tariff_amount,
-                $device->company_id
-            );
-            $this->publishMessage($device->dev_id, $payload);
-        } else {
-            $this->noMoney($device->dev_id);
-        }
+                $this->handleOpMode($device->op_mode, $user, $device, $data);
  
-  
+            }
         
  
         }
