717a890d8155c59f6c395575ec303a563b46d792
vindexTOS <giorgikutateladze1998@gmail.com>
2024-10-08 15:02:28 +0400

    remoe sub fuction

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,16 +13,16 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->command('transaction:details')->everyFiveMinutes();
-        $schedule->command('user:subscription-check')->everyTwoHours();
-        $schedule->command('user:subscription-check')->dailyAt('00:00')
-        ->timezone('Asia/Tbilisi');
-        $schedule->command('user:subscription-check')->dailyAt('00:01')
-        ->timezone('Asia/Tbilisi');
-        $schedule->command('user:subscription-check')->dailyAt('00:05')
-        ->timezone('Asia/Tbilisi');;
-        $schedule->command('user:subscription-check')
-        ->dailyAt('04:05')
-        ->timezone('Asia/Tbilisi');
+        // $schedule->command('user:subscription-check')->everyTwoHours();
+        // $schedule->command('user:subscription-check')->dailyAt('00:00')
+        // ->timezone('Asia/Tbilisi');
+        // $schedule->command('user:subscription-check')->dailyAt('00:01')
+        // ->timezone('Asia/Tbilisi');
+        // $schedule->command('user:subscription-check')->dailyAt('00:05')
+        // ->timezone('Asia/Tbilisi');;
+        // $schedule->command('user:subscription-check')
+        // ->dailyAt('04:05')
+        // ->timezone('Asia/Tbilisi');
         
     }
     
