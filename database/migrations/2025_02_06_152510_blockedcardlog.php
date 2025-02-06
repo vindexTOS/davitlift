3fffaa2900c84b68c77e64289b57413fa51e82d6
vindexTOS <giorgikutateladze1998@gmail.com>
2025-02-06 15:37:52 +0400

    changing migration probably

--- database/migrations/2025_02_06_152510_blockedcardlog.php
+++ database/migrations/2025_02_06_152510_blockedcardlog.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create("blockedcardlog", function (Blueprint $table) {
+        Schema::create("blockedcardlogs", function (Blueprint $table) {
             $table->id();
 
 
